d6a2b93341775efcd9afc0d2bf5d89f1968c62b3
ratan.aniket09@gmail.com <ratan.aniket09@gmail.com>
2024-06-25 16:49:20 +0530

    Added Heading name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import TechSkillSection from "@/components/TechSkillSection";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      {/* <h1 className="text-3xl text-center font-bold text-white">Aniket Ratan.</h1> */}
+      <h1 className="text-4xl text-center font-bold text-white">Aniket Ratan.</h1>
       <HeroSection />
       <AboutSection />
       <SkillSection />
